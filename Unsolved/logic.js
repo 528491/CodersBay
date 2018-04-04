f7166c8c2aecdbbae4015653e13d485f2a079329
unknown <mattcmej@gmail.com>
2018-04-04 12:33:24 -0700

    Initialize firebase on html and logic.js files.

--- Unsolved/logic.js
+++ Unsolved/logic.js
@@ -1,6 +1,15 @@
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
 // (Ex. 3.0 != 3.7.0)
+var config = {
+  apiKey: "AIzaSyAs-neWRcd7syXn7N0aKcbsXNYRXg_DXdg",
+  authDomain: "harry-clutter-b7ae6.firebaseapp.com",
+  databaseURL: "https://harry-clutter-b7ae6.firebaseio.com",
+  projectId: "harry-clutter-b7ae6",
+  storageBucket: "harry-clutter-b7ae6.appspot.com",
+  messagingSenderId: "605667578097"
+};
+firebase.initializeApp(config);
 
 
 // Assign the reference to the database to a variable named 'database'
